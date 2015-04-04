39e033a4556882b8fa3c6874e062c26c0a55428f
Connor Smith <cjs.smith1993@gmail.com>
2015-04-04 14:20:09 -0600

    moved simulation models into their own file

--- reliabilitySimulations.py
+++ reliabilitySimulations.py
@@ -2,75 +2,10 @@ import numpy as np
 from random import randint
 import copy
 
-import os
-import struct
-
 from multiprocessing import Pool
 
 FAILED = False
 WORKING = True
-
-randomGenerator = open("/dev/urandom", "rb")
-MAX_SHORT = 2**16
-
-class SimulationSystem:
-	UPPER_BOUND = 10000
-
-	def __init__(self, numComponents, probabilityOfFailure, probabilityOfRepair):
-		self.numComponents = numComponents
-		self.components = [WORKING for i in range(numComponents)]
-		self.probabilityOfFailure = probabilityOfFailure
-		self.probabilityOfRepair = probabilityOfRepair
-
-	def eventHappened(self, probability):
-		return struct.unpack("H", randomGenerator.read(2))[0] / MAX_SHORT < probability
-
-	def evolve(self, t):
-		for idx in range(len(self.components)):
-			if self.components[idx] is WORKING:
-				if self.eventHappened(self.probabilityOfFailure):
-					self.components[idx] = FAILED
-			else:
-				if self.eventHappened(self.probabilityOfRepair):
-					self.components[idx] = WORKING
-
-class SimplexSystem(SimulationSystem):
-	def __init__(self, probabilityOfFailure, probabilityOfRepair):
-		super(SimplexSystem, self).__init__(1, probabilityOfFailure, probabilityOfRepair)
-
-	def isWorking(self):
-		return self.components[0] is WORKING
-
-class SeriesSystem(SimulationSystem):
-	def __init__(self, numComponents, probabilityOfFailure, probabilityOfRepair):
-		super(SeriesSystem, self).__init__(
-			numComponents,
-			probabilityOfFailure,
-			probabilityOfRepair)
-
-	def isWorking(self):
-		return self.components.count(WORKING) == self.numComponents
-
-class ParallelSystem(SimulationSystem):
-	def __init__(self, numComponents, probabilityOfFailure, probabilityOfRepair):
-		super(ParallelSystem, self).__init__(
-			numComponents,
-			probabilityOfFailure,
-			probabilityOfRepair)
-
-	def isWorking(self):
-		return self.components.count(WORKING) > 0
-
-class NMRSystem(SimulationSystem):
-	def __init__(self, numComponents, requiredNumComponents, probabilityOfFailure, probabilityOfRepair):
-		super(NMRSystem, self).__init__(
-			numComponents,
-			probabilityOfFailure,
-			probabilityOfRepair)
-		self.requiredNumComponents = requiredNumComponents
-
-	def isWorking(self):
-		return self.components.count(WORKING) >= self.requiredNumComponents
 
 class Simulator:
 	def __init__(self, numIterations, populationSize, timeArray):
